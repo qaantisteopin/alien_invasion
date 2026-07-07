c576a9d1b139fd400dc65fe60f72b815fd2efc38
Voroshilov.DO <Voroshilov.DO@gazprom-neft.ru>
2026-07-08 00:28:08 +0400

    feat(alien_inasion): Создал корабль, настройки и игровой ресурс

---
+++ alien_invasion.py
@@ -0,0 +1,41 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, 
+                                               self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+    
+    def run_game(self):
+        """Запускает основной цикл игры."""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+    
+    def _check_events(self):
+        """Обрабатывает нажатие клавиш и события мыши."""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+    
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
